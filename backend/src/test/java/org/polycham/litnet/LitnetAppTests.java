05b41612cabedb8acc52ab16b86efb8a6969a10e
polyglot-chameleon <oleg.harlamov@web.de>
2024-09-10 20:32:09 +0200

    setup testing #1

---
+++ backend/src/test/java/org/polycham/litnet/LitnetAppTests.java
@@ -0,0 +1,13 @@
+package org.polycham.litnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitnetAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
