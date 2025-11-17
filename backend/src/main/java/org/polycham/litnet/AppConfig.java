a050899a46be50be2ada0bf039c9ec22d4db3d3f
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2025-11-17 10:25:56 +0100

    16 dockerize (#17)
    * setup containerized gradle
    
    * fix db conn
    
    * pull frontend build into spring

--- backend/src/main/java/org/polycham/litnet/AppConfig.java
+++ backend/src/main/java/org/polycham/litnet/AppConfig.java
@@ -8,29 +8,44 @@ import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider;
 import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
 @EnableReactiveNeo4jRepositories(basePackages = "repos")
+@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean(name = "reactiveTransactionManager")
-    public ReactiveNeo4jTransactionManager reactiveTransactionManager(Driver driver,
-            ReactiveDatabaseSelectionProvider databaseNameProvider) {
-        return new ReactiveNeo4jTransactionManager(driver, databaseNameProvider);
+    public ReactiveNeo4jTransactionManager reactiveTransactionManager(
+        Driver driver,
+        ReactiveDatabaseSelectionProvider databaseNameProvider
+    ) {
+        return new ReactiveNeo4jTransactionManager(
+            driver,
+            databaseNameProvider
+        );
     }
 
     @Bean
     public Configuration cypherDslConfiguration() {
-        return Configuration.newConfig()
-                .withDialect(Dialect.NEO4J_5).build();
+        return Configuration.newConfig().withDialect(Dialect.NEO4J_5).build();
     }
 
     /* meh */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+        registry
+            .addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedOrigins("*");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/");
+    }
 }
