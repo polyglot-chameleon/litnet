0950e1af44aeeb0d19a5736329a6485f54035495
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2025-11-18 07:39:32 +0100

    fix frontend serve (#19)
    * add view controller forwarding #18
    
    * drop frontend api url #18

--- backend/src/main/java/org/polycham/litnet/AppConfig.java
+++ backend/src/main/java/org/polycham/litnet/AppConfig.java
@@ -7,20 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider;
 import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @org.springframework.context.annotation.Configuration
 @EnableReactiveNeo4jRepositories(basePackages = "repos")
-@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean(name = "reactiveTransactionManager")
     public ReactiveNeo4jTransactionManager reactiveTransactionManager(
-        Driver driver,
-        ReactiveDatabaseSelectionProvider databaseNameProvider
+        final Driver driver,
+        final ReactiveDatabaseSelectionProvider databaseNameProvider
     ) {
         return new ReactiveNeo4jTransactionManager(
             driver,
@@ -33,19 +31,15 @@ public class AppConfig implements WebMvcConfigurer {
         return Configuration.newConfig().withDialect(Dialect.NEO4J_5).build();
     }
 
-    /* meh */
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedOrigins("*");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry
             .addResourceHandler("/**")
             .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 }
