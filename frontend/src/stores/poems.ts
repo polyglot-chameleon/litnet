70e932478c12fa4443bb7cbf61ddfd7ed001084c
polyglot-chameleon <oleg.harlamov@web.de>
2025-11-18 07:28:23 +0100

    drop frontend api url #18

--- frontend/src/stores/poems.ts
+++ frontend/src/stores/poems.ts
@@ -6,27 +6,33 @@ export const usePoemStore = defineStore('poems', () => {
   const poems = ref<Poem[]>([])
   const currentPoem = ref<Poem>(initPoem)
   const searchResults = ref<Poem[]>([])
-  const searchTerm = ref<string>("")
-
+  const searchTerm = ref<string>('')
 
   const getAllPoems = async () => {
-    const resp = await fetch(`${import.meta.env.VITE_API_URL}/poems`)
+    const resp = await fetch(`/poems`)
     poems.value = await resp.json()
   }
 
-  const getPoemById = async (id: string ) => {
-      poems.value.length ? currentPoem.value = poems.value.find(p => p.id === id)! : fetch(`${import.meta.env.VITE_API_URL}/poems/${id}`).then(resp => resp.json()).then(data => currentPoem.value = data)
+  const getPoemById = async (id: string) => {
+    poems.value.length
+      ? (currentPoem.value = poems.value.find((p) => p.id === id)!)
+      : fetch(`/poems/${id}`)
+          .then((resp) => resp.json())
+          .then((data) => (currentPoem.value = data))
   }
 
   const searchPoem = async (term: string) => {
     searchTerm.value = term
 
-    const resp = await fetch(`${import.meta.env.VITE_API_URL}/poems/search`, {method: "post", headers: {
-      'Content-Type': 'application/json'
-    }, body: JSON.stringify({content: term} satisfies Partial<Poem>)})
+    const resp = await fetch(`/poems/search`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content: term } satisfies Partial<Poem>)
+    })
     searchResults.value = await resp.json()
   }
 
   return { poems, getAllPoems, currentPoem, getPoemById, searchTerm, searchResults, searchPoem }
 })
-
